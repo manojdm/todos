391f94096d98c7481fa727bc467275aad176345b
manojdm <manumanoj709@gmail.com>
2023-06-27 22:30:39 +0530

    feat: buttons functionality working

--- pages/scripts/timer.js
+++ pages/scripts/timer.js
@@ -21,46 +21,6 @@ let bconsumed = 0;
 
 let current = 'pomodoro'
 
-//Timer function
-const timerFunction = () => {
-    if(current == 'pomodoro'){
-        if(secLeft == 0){
-            secLeft = 59
-            minLeft -= 1
-            if(minLeft == 0){
-                pconsumed += 1;
-                minLeft = bmaxMinutes;
-                current = 'break'
-            }
-            minutes.innerHTML = minLeft.toString().padStart(2, '0')
-
-        } else {
-            secLeft -=1
-        }
-
-        seconds.innerHTML=secLeft.toString().padStart(2, '0');
-
-    } else {
-        
-        if(secLeft == 0){
-            secLeft = 59
-            minLeft -= 1
-            if(minLeft == 0){
-                bconsumed += 1
-                minLeft = pmaxMinutes;
-                current = 'pomodoro'
-            }
-            minutes.innerHTML = minLeft.toString().padStart(2, '0')
-
-        } else {
-            secLeft -=1
-        }
-
-        seconds.innerHTML=secLeft.toString().padStart(2, '0');
-    } 
-
-}
-
 export default timerFunction
 
 // window.addEventListener('load', () => {
--- pages/todo/[id].tsx
+++ pages/todo/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
 import { useSearchParams } from 'next/navigation'
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from '@apollo/client';
@@ -13,7 +13,12 @@ const client = new ApolloClient({
 const Todo = () => {
     //refs
     const startTodoBtn = useRef<HTMLDivElement | null>(null);
-    const timer = useRef<HTMLDivElement | null>(null)
+    const timer = useRef<HTMLDivElement | null>(null);
+    const pause = useRef<HTMLDivElement | null>(null);
+    const play = useRef<HTMLDivElement | null>(null);
+
+    //timer state
+    const [running, setRunning] = useState(false)
 
     //get query id
     const router = useRouter();
@@ -54,6 +59,85 @@ const Todo = () => {
     const {loading, error, data, refetch} = useQuery(GET_TODO);
     const [updateTodo] = useMutation(UPDATE_TODO);
 
+        //Timer function
+    const timerFunction = () => {
+        const timer = document.querySelector('.timer');
+        const minutes = document.querySelector('.minutes');
+        const seconds = document.querySelector('.seconds');
+
+        //Pomodoro
+        const pmaxMinutes = 24;
+
+        //break
+        const bmaxMinutes = 4;
+
+        const minMinutes = 0;
+        const minSeconds = 0;
+
+        //time left
+        let minLeft = 24;
+        let secLeft = 0;
+
+        //Total consumptions
+        let pconsumed = 0;
+        let bconsumed = 0;
+
+        let current = 'pomodoro'
+
+        if(current == 'pomodoro'){
+            if(secLeft == 0){
+                secLeft = 59
+                minLeft -= 1
+                if(minLeft == 0){
+                    pconsumed += 1;
+                    minLeft = bmaxMinutes;
+                    current = 'break'
+                }
+                minutes.innerHTML = minLeft.toString().padStart(2, '0')
+
+            } else {
+                secLeft -=1
+            }
+
+            seconds.innerHTML=secLeft.toString().padStart(2, '0');
+
+        } else {
+            
+            if(secLeft == 0){
+                secLeft = 59
+                minLeft -= 1
+                if(minLeft == 0){
+                    bconsumed += 1
+                    minLeft = pmaxMinutes;
+                    current = 'pomodoro'
+                }
+                minutes.innerHTML = minLeft.toString().padStart(2, '0')
+
+            } else {
+                secLeft -=1
+            }
+
+            seconds.innerHTML=secLeft.toString().padStart(2, '0');
+        } 
+
+    }
+
+
+    useEffect(() => {
+        if(running){
+            if(play.current && pause.current){
+                play.current.style.display = 'none';
+                pause.current.style.display = 'block';
+            }
+            setInterval(timerFunction, 1000)
+        } else {
+            if(pause.current && play.current) {
+                pause.current.style.display = 'none';
+                play.current.style.display = 'block';
+            }
+        }
+    },[running])
+
     if(loading){
         return('Loading...');
     }
@@ -84,36 +168,43 @@ const Todo = () => {
         }
       };
       
-      useEffect(() => {
-        if(startTodoBtn.current) {
-            //Event listener
-            startTodoBtn.current.addEventListener('click', () => {
-                console.log('wow')
-            })
+      const handleStartTodo = () => {
+        if (startTodoBtn.current && timer.current) {
+          startTodoBtn.current.style.display = 'none';
+          timer.current.style.display = 'block';
+          setRunning(true)
         }
+      };
+      
+      const handlePause = () => {
+        setRunning(false)
+      }
 
-      }, [])
-
+      const handlePlay = () => {
+        setRunning(true)
+      }
+ 
   return (
 <div className="todo-section card">
-    {!loading && data ? <>
-<div ref={startTodoBtn} className="todo-start">
+    {!loading &&  data ? <>
+{!todo.completed &&
+<> <div ref={startTodoBtn} onClick={handleStartTodo} className="todo-start">
     Start Todo
 </div>
 <div ref={timer} className="todo-timer hide">
-<div className="count timer">
-    <span className="minutes">25</span> : <span className="seconds">00</span>
-</div>
-<div className="timer-actions">
-    <div className="pause">
-        <i className="fa-solid fa-pause"></i>
-        <i className="fa-solid fa-play"></i>
+    <div className="count timer">
+        <span className="minutes">25</span> : <span className="seconds"></span>
     </div>
-    <div className="stop">
-        <i className="fa-solid fa-stop"></i>
-    </div>
-</div>
-</div>
+    <div className="timer-actions">
+        <div className="pause">
+            <i onClick={handlePause} ref={pause}  className="fa-solid fa-pause"></i>
+            <i onClick={handlePlay} ref={play} className="fa-solid fa-play"></i>
+        </div>
+        <div className="stop">
+            <i className="fa-solid fa-stop"></i>
+        </div>
+    </div> 
+</div> </>}
 <div className="title">
 Todo: {todo.name}
 </div>
