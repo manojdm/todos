e2046d73345ba71738d8db76c47d69d7bb01e18e
manojdm <manumanoj709@gmail.com>
2023-06-27 23:34:59 +0530

    feat: pomodoro working

--- pages/todo/[id].tsx
+++ pages/todo/[id].tsx
@@ -1,248 +1,280 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useRouter } from 'next/router'
-import { useSearchParams } from 'next/navigation'
+import React, { use, useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useSearchParams } from 'next/navigation';
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from '@apollo/client';
-import timerFunction from '../scripts/timer'
 
 // Set up Apollo Client
 const client = new ApolloClient({
-    uri: '/api/graphql',
-    cache: new InMemoryCache(),
+  uri: '/api/graphql',
+  cache: new InMemoryCache(),
 });
 
 const Todo = () => {
-    //refs
-    const startTodoBtn = useRef<HTMLDivElement | null>(null);
-    const timer = useRef<HTMLDivElement | null>(null);
-    const pause = useRef<HTMLDivElement | null>(null);
-    const play = useRef<HTMLDivElement | null>(null);
+  // refs
+  const startTodoBtn = useRef<HTMLDivElement | null>(null);
+  const timer = useRef<HTMLDivElement | null>(null);
+  const pause = useRef<HTMLDivElement | null>(null);
+  const play = useRef<HTMLDivElement | null>(null);
+  const stop = useRef<HTMLDivElement | null>(null);
 
-    //timer state
-    const [running, setRunning] = useState(false)
+  // timer state
+  const [running, setRunning] = useState(false);
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
 
-    //get query id
-    const router = useRouter();
-    const id = router.query.id;
+  //Tomatoes consumed
+  const [tomatoesConsumed, setTomatoesConsumed] = useState(0)
 
-    //get search params
-    const params = useSearchParams();
-    const type = params.get('type')
+  // current state
+  const [current, setCurrent] = useState<'pomodoro' | 'break'>('pomodoro');
 
-    //Query
-    const GET_TODO = gql`
+  // get query id
+  const router = useRouter();
+  const id = router.query.id;
+
+  // get search params
+  const params = useSearchParams();
+  const type = params.get('type');
+
+  // Query
+  const GET_TODO = gql`
     {
-        todo(id: ${id}) {
-        id,
-        name,
-        description,
-        completed,
-        dueDate,
-        tomatoesConsumed
-        }
-    }
-    `;
-
-    const UPDATE_TODO = gql`
-    mutation UpdateTodo($id: ID!, $completed: Boolean!) {
-      updateTodo(id: $id, completed: $completed) {
+      todo(id: ${id}) {
         id
         name
-        completed
         description
+        completed
         dueDate
         tomatoesConsumed
       }
     }
   `;
+
+  const UPDATE_TODO = gql`
+  mutation UpdateTodo($id: ID!, $completed: Boolean!, $tomatoesConsumed: Int!) {
+    updateTodo(id: $id, completed: $completed, tomatoesConsumed: $tomatoesConsumed) {
+      id
+      name
+      completed
+      description
+      dueDate
+      tomatoesConsumed
+    }
+  }
+`;
+
+  const { loading, error, data, refetch } = useQuery(GET_TODO);
+  const [updateTodo] = useMutation(UPDATE_TODO);
+
+  const timerFunction = () => {
+    const pmaxMinutes = 25;
+    const bmaxMinutes = 5;
+  
+    if (seconds === 0) {
+      if (minutes === 0) {
+        if (current === 'pomodoro') {
+          setTomatoesConsumed((prev) => prev+1)
+          setMinutes(bmaxMinutes);
+          setCurrent('break');
+        } else {
+          setMinutes(pmaxMinutes);
+          setCurrent('pomodoro');
+        }
+      } else {
+        setMinutes((prevMinutes) => prevMinutes - 1);
+        setSeconds(59);
+      }
+    } else {
+      setSeconds((prevSeconds) => prevSeconds - 1);
+    }
+  };
   
   
-    const {loading, error, data, refetch} = useQuery(GET_TODO);
-    const [updateTodo] = useMutation(UPDATE_TODO);
 
-        //Timer function
-    const timerFunction = () => {
-        const timer = document.querySelector('.timer');
-        const minutes = document.querySelector('.minutes');
-        const seconds = document.querySelector('.seconds');
-
-        //Pomodoro
-        const pmaxMinutes = 24;
-
-        //break
-        const bmaxMinutes = 4;
-
-        const minMinutes = 0;
-        const minSeconds = 0;
-
-        //time left
-        let minLeft = 24;
-        let secLeft = 0;
-
-        //Total consumptions
-        let pconsumed = 0;
-        let bconsumed = 0;
-
-        let current = 'pomodoro'
-
-        if(current == 'pomodoro'){
-            if(secLeft == 0){
-                secLeft = 59
-                minLeft -= 1
-                if(minLeft == 0){
-                    pconsumed += 1;
-                    minLeft = bmaxMinutes;
-                    current = 'break'
-                }
-                minutes.innerHTML = minLeft.toString().padStart(2, '0')
-
-            } else {
-                secLeft -=1
-            }
-
-            seconds.innerHTML=secLeft.toString().padStart(2, '0');
-
-        } else {
-            
-            if(secLeft == 0){
-                secLeft = 59
-                minLeft -= 1
-                if(minLeft == 0){
-                    bconsumed += 1
-                    minLeft = pmaxMinutes;
-                    current = 'pomodoro'
-                }
-                minutes.innerHTML = minLeft.toString().padStart(2, '0')
-
-            } else {
-                secLeft -=1
-            }
-
-            seconds.innerHTML=secLeft.toString().padStart(2, '0');
-        } 
-
-    }
-
-
-    useEffect(() => {
-        if(running){
-            if(play.current && pause.current){
-                play.current.style.display = 'none';
-                pause.current.style.display = 'block';
-            }
-            setInterval(timerFunction, 1000)
-        } else {
-            if(pause.current && play.current) {
-                pause.current.style.display = 'none';
-                play.current.style.display = 'block';
-            }
-        }
-    },[running])
-
-    if(loading){
-        return('Loading...');
-    }
-
-    if(error){
-        return ('Todo not found.....');
-    }
-
-    const todo = data.todo;
-
-    //Mark complete Event listener
-    const handleMarkComplete = async (e:any, id: string | number) => {
-        e.preventDefault();
-
-        try {
-          const { data } = await updateTodo({
-            variables: {
-              id: String(id), // Convert id to string
-              completed: true,
-            },
-          });
-      
-          if (data.updateTodo.completed) {
-            refetch();
-          }
-        } catch (e: any) {
-          return 'Issue with marking the todo complete';
-        }
-      };
-      
-      const handleStartTodo = () => {
-        if (startTodoBtn.current && timer.current) {
-          startTodoBtn.current.style.display = 'none';
-          timer.current.style.display = 'block';
-          setRunning(true)
-        }
-      };
-      
-      const handlePause = () => {
-        setRunning(false)
+  useEffect(() => {
+    let interval: any;
+  
+    if (running) {
+      if (play.current && pause.current) {
+        play.current.style.display = 'none';
+        pause.current.style.display = 'block';
       }
-
-      const handlePlay = () => {
-        setRunning(true)
+  
+      interval = setInterval(timerFunction, 10);
+    } else {
+      if (pause.current && play.current) {
+        pause.current.style.display = 'none';
+        play.current.style.display = 'block';
       }
+  
+      clearInterval(interval);
+    }
+  
+    // Clearing the interval on cleanup
+    return () => {
+      clearInterval(interval);
+    };
+  }, [running, minutes, seconds]);
+  
+
+  if (loading) {
+    return 'Loading...';
+  }
+
+  if (error) {
+    return 'Todo not found...';
+  }
+
+  const todo = data.todo;
+
+  // Mark complete Event listener
+  const handleMarkComplete = async (e: any, id: string | number) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await updateTodo({
+        variables: {
+          id: String(id), // Convert id to string
+          completed: true,
+          tomatoesConsumed
+        },
+      });
+
+      if (data.updateTodo.completed) {
+        refetch();
+      }
+    } catch (e: any) {
+      return 'Issue with marking the todo complete';
+    }
+  };
+
+  const handleStartTodo = () => {
+    if (startTodoBtn.current && timer.current) {
+      startTodoBtn.current.style.display = 'none';
+      timer.current.style.display = 'block';
+      setRunning(true);
+    }
+  };
+
+  const handlePause = () => {
+    setRunning(false);
+  };
+
+  const handlePlay = () => {
+    setRunning(true);
+  };
+
+  const handleReset = () => {
+    setRunning(false);
+    setMinutes(25);
+    setSeconds(0);
+    setTomatoesConsumed(0);
+  }
  
   return (
-<div className="todo-section card">
-    {!loading &&  data ? <>
-{!todo.completed &&
-<> <div ref={startTodoBtn} onClick={handleStartTodo} className="todo-start">
-    Start Todo
-</div>
-<div ref={timer} className="todo-timer hide">
-    <div className="count timer">
-        <span className="minutes">25</span> : <span className="seconds"></span>
+    <div className="todo-section card">
+      {!loading && data ? (
+        <>
+          <div className="title">Todo: {todo.name}</div>
+          {!todo.completed && (
+            <>
+              <div ref={startTodoBtn} onClick={handleStartTodo} className="todo-start">
+                Start Todo
+              </div>
+              <div ref={timer} className="todo-timer hide">
+                <div className="timer-status">
+                    Status: {current == 'pomodoro' ? 'Focus Time' : 'Break Time'}
+                </div>
+                <div className="count timer">
+                  <span className="minutes">{minutes.toString().padStart(2, '0')}</span> :{' '}
+                  <span className="seconds">{seconds.toString().padStart(2, '0')}</span>
+                </div>
+                <div className="timer-actions">
+                  <div className="pause">
+                    <i onClick={handlePause} ref={pause} className="fa-solid fa-pause"></i>
+                    <i onClick={handlePlay} ref={play} className="fa-solid fa-play"></i>
+                  </div>
+                  <div className="reset">
+                      <i onClick={handleReset} className="fa-sharp fa-solid fa-arrow-rotate-left"></i>
+                  </div>
+                  <div className="stop">
+                    <i onClick={(e) => handleMarkComplete(e, todo.id)} ref={stop} className="fa-solid fa-stop"></i>
+                  </div>
+                </div>
+                <div className="message">
+                    Please don't close this window
+                </div>
+              </div>
+            </>
+          )}
+          <div className="todo-content">
+            <div className="todo-details">
+              <div className="created">Due Date: {todo.dueDate}</div>
+              <div className="tomatoes">
+                <i className="fa-solid fa-apple-whole"></i>: {todo.completed ? todo.tomatoesConsumed : tomatoesConsumed}
+              </div>
+            </div>
+            <div className="todo-editor">
+              <div className="form-container">
+                <form>
+                  <div className="form-div title-div">
+                    <label>Title</label>
+                    <input
+                      className="title-input"
+                      type="text"
+                      value={todo.name}
+                      disabled={type === 'edit' ? false : true}
+                    />
+                  </div>
+                  <div className="form-div description">
+                    <label>description</label>
+                    <input
+                      className="description-input"
+                      type="text"
+                      value={todo.description}
+                      disabled={type === 'edit' ? false : true}
+                    />
+                  </div>
+                  <div className="form-div due-date">
+                    <label>Due date</label>
+                    <input
+                      type="date"
+                      className="duedate"
+                      value={todo.dueDate}
+                      disabled={type === 'edit' ? false : true}
+                    />
+                  </div>
+                  <div className="form-div form-cta update hide">
+                    <input type="submit" value="update" disabled={type === 'edit' ? false : true} />
+                  </div>
+                  {!todo.completed ? (
+                    <div className="form-div form-cta complete">
+                      <input
+                        onClick={(e) => handleMarkComplete(e, todo.id)}
+                        className="complete"
+                        type="submit"
+                        value="mark complete"
+                      />
+                    </div>
+                  ) :
+                  <div className="form-div form-cta complete">
+                    <input
+                        className="complete"
+                        type="submit"
+                        value="completed"
+                        disabled
+                    />
+                  </div>}
+                </form>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        'Todo not found'
+      )}
     </div>
-    <div className="timer-actions">
-        <div className="pause">
-            <i onClick={handlePause} ref={pause}  className="fa-solid fa-pause"></i>
-            <i onClick={handlePlay} ref={play} className="fa-solid fa-play"></i>
-        </div>
-        <div className="stop">
-            <i className="fa-solid fa-stop"></i>
-        </div>
-    </div> 
-</div> </>}
-<div className="title">
-Todo: {todo.name}
-</div>
-<div className="todo-content">
-<div className="todo-details">
-    <div className="created">Due Date: {todo.dueDate}</div>
-    <div className="tomatoes"><i className="fa-solid fa-apple-whole"></i>: {todo.tomatoesConsumed}</div>
-</div>
-<div className="todo-editor">
-    <div className="form-container">
-        <form>
-            <div className="form-div title-div">
-                <label>Title</label>
-                <input className="title-input" type="text" value={todo.name} disabled={type == 'edit' ? false : true} />
-            </div>
-            <div className="form-div description">
-                <label>description</label>
-                <input className="description-input" type="text" value={todo.description} disabled={type == 'edit' ? false : true} />
-            </div>
-            <div className="form-div due-date">
-                <label>Due date</label>
-                <input type="date" className="duedate" value={todo.dueDate}  disabled={type == 'edit' ? false : true} />
-            </div>
-            <div className="form-div form-cta update hide">
-                <input type="submit" value="update" disabled={type == 'edit' ? false : true} />
-            </div>
-            {!todo.completed ?
-            <div className="form-div form-cta complete">
-                <input onClick={(e) => handleMarkComplete(e, todo.id)} className="complete" type="submit" value="mark complete" />
-            </div>: ''}
-        </form>
-    </div>
-</div>
-</div> </> : 'Todo not found'
-}
-        
-</div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
